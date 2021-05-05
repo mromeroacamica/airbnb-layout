412284a9ed808d42632a9af5df1404461c03531c
MatiRomero <mgr_89@hotmail.com>
2021-05-05 10:15:49 -0300

    ICONOS ANDROID

--- ios/JornalYaTests/JornalYaTests.m
+++ ios/JornalYaTests/JornalYaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface layoutsTests : XCTestCase
+@interface JornalYaTests : XCTestCase
 
 @end
 
-@implementation layoutsTests
+@implementation JornalYaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
